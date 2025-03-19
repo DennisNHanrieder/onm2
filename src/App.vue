db796cd9d1520c8dda6ed93536b49f09f9ecc714
Dennsi <mr.dusky@outlook.de>
2025-03-19 10:38:13 +0100

    base version

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import {ref} from 'vue'
+import Entry from './modules/Entry'
+import MyLi from './components/MyLi.vue'
+
+const contents = ref<string>("")
+const entryData = ref<Entry[]>([])
+
+function saveEntry(): void {
+  const entry: Entry = new Entry(new Date(), contents.value)
+
+  entryData.value.push(entry)
+  contents.value = ""
+}
+
+function deleteEntry(index: number): void {
+  entryData.value.splice(index,1)
+}
+
+function updateEntry(index: number, entry: Entry): void {
+  entryData.value[index] = entry
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
+<ul>
+  <MyLi v-for="(entry,index) in entryData" 
+    :date = "entry.dateStr"
+    :contents = "entry.contents"
+    :key = "entry.dateStr"
+    @delete-me="deleteEntry(index)"
+    @save-me="updateEntry(index, $event)"
+  />
+
+</ul>
+
+<textarea v-model="contents"></textarea><br>
+<button @click="saveEntry">Save</button>
+
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
